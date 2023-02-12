12ed3be0fa9ae41c9a83c32ff65abefa3116e7b8
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-13 01:55:46 +0300

    Diffrent way

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,10 +29,10 @@ end
 
 def retrieve_first_element_from_array(array)
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[0]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[-1]
+  array.last
 end
